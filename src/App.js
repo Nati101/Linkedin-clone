5349ae824436f70cb59485cb3a708873c74ba8b8
Nati101 <71053656+Nati101@users.noreply.github.com>
2021-05-22 21:13:25 -0600

    login using google update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
-import './App.css';
-import Login from "./components/Login"
-import Header from "./components/Header"
+import { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Login";
+import Header from "./components/Header";
 import Home from "./components/Home";
-
-function App() {
+import { getUserAuth } from "./actions";
+import { connect } from "react-redux";
+function App(props) {
+  useEffect(() => {
+    props.getUserAuth();
+  }, []);
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path='/'>
-            <Login/>
+          <Route exact path="/">
+            <Login />
           </Route>
-          <Route path='/home'>
-            <Header/>
-            <Home/>
+          <Route path="/home">
+            <Header />
+            <Home />
           </Route>
         </Switch>
       </Router>
@@ -22,4 +27,12 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  getUserAuth: () => dispatch(getUserAuth()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,13 @@ import styled from "styled-components";
 import Leftside from "./Leftside";
 import Main from "./Main";
 import Rightside from "./Rightside";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
 
 const Home = (props) => {
   return (
     <Container>
+      {!props.user && <Redirect to="/" />}
       <Section>
         <h5>
           <a>Hiring in a hurry? - </a>
@@ -76,4 +79,11 @@ const Layout = styled.div`
     padding: 0 5px;
   }
 `;
-export default Home;
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.userState.user,
+  };
+};
+
+export default connect(mapStateToProps)(Home);
