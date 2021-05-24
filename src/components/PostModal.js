55e38f4047367ed59cfce25d934fb88e80aa8f28
Nati101 <71053656+Nati101@users.noreply.github.com>
2021-05-24 00:44:08 -0600

    text area added

--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const PostModal = (props) => {
+  const [editorText, setEditorText] = useState("");
   return (
     <Container>
       <Content>
@@ -10,6 +12,36 @@ const PostModal = (props) => {
             <img src="/images/close-icon.svg" alt="" />
           </button>
         </Header>
+        <SharedContent>
+          <UserInfo>
+            <img src="/images/user.svg" alt="" />
+            <span>Name</span>
+          </UserInfo>
+          <textarea
+            value={editorText}
+            onChange={(e) => setEditorText(e.target.value)}
+            placeholder="What do you want to talk about?"
+            autoFocus={true}
+          ></textarea>
+        </SharedContent>
+        <SharedCreation>
+          <AttachAssets>
+            <AssetButton>
+              <img src="/images/share-img.svg" alt="" />
+            </AssetButton>
+            <AssetButton>
+              <img src="/images/share-video.svg" alt="" />
+            </AssetButton>
+          </AttachAssets>
+          <ShareComment>
+            <AssetButton>
+              <img src="/images/share-comment.svg" alt="" />
+              Anyone
+            </AssetButton>
+          </ShareComment>
+
+          <PostButton>Post</PostButton>
+        </SharedCreation>
       </Content>
     </Container>
   );
@@ -40,4 +72,100 @@ const Content = styled.div`
   margin: 0 auto;
 `;
 
+const Header = styled.div`
+  display: block;
+  padding: 16px 20px;
+  border: 1px solid rgba(0, 0, 0, 0.15);
+  font-size: 16px;
+  line-height: 1.5;
+  color: rgba(0, 0, 0, 0.6);
+  font-weight: 400;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  button {
+    height: 40px;
+    width: 40px;
+    color: rgba(0, 0, 0, 0.15);
+    svg {
+      pointer-events: none;
+    }
+  }
+`;
+
+const SharedContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  overflow-y: auto;
+  vertical-align: baseline;
+  background: transparent;
+  padding: 8px 12px;
+`;
+
+const UserInfo = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 12px 12px;
+  svg,
+  img {
+    width: 48px;
+    height: 48px;
+    background-clip: content-box;
+    border: 2px solid transparent;
+    border-radius: 50%;
+  }
+  span {
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 1.5;
+    margin-left: 5px;
+  }
+`;
+
+const SharedCreation = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 12px 24px 12px 16px;
+`;
+
+const AssetButton = styled.button`
+  display: flex;
+  align-items: center;
+  height: 40px;
+  min-width: auto;
+  color: rgba(0, 0, 0, 0.5);
+`;
+
+const AttachAssets = styled.div`
+  align-items: center;
+  display: flex;
+  padding-right: 8px;
+  ${AssetButton} {
+    width: 40px;
+  }
+`;
+
+const ShareComment = styled.div`
+  padding-left: 8px;
+  margin-right: auto;
+  border-left: 1px solid rgba(0, 0, 0, 0.15);
+  ${AssetButton} {
+    svg {
+      margin-right: 5px;
+    }
+  }
+`;
+
+const PostButton = styled.button`
+  min-width: 60px;
+  border-radius: 20px;
+  padding-left: 16px;
+  padding-right: 16px;
+  background: #0a66c2;
+  color: white;
+  &:hover {
+    background: #004182;
+  }
+`;
 export default PostModal;
